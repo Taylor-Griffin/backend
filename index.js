fececddc9d3bb06d17b3356264ebb5cf16f97193
Taylor Griffin <tmgriffin73@gmail.com>
2022-07-25 16:46:42 -0400

    divide app into modules

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const logger = require('./utils/logger');
 
 const server = http.createServer(app);
 
-server.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+server.listen(process.env.PORT, () => {
+  logger.info(`Server running on port ${process.env.PORT}`);
 });
